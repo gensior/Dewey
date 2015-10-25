816b28fd2bf275210dff3d2abd49352fbe63bd43
Jesse Franceschini <j.franceschini@gmail.com>
2015-10-24 19:08:01 -0700

    Updates LibraryDriver.

--- src/com/jfranceschini/LibraryDriver.java
+++ src/com/jfranceschini/LibraryDriver.java
@@ -3,6 +3,7 @@ package com.jfranceschini;
 import java.util.Date;
 
 import com.jfranceschini.library.Book;
+import com.jfranceschini.library.LibraryMember;
 
 public class LibraryDriver {
 	public static void main(String[] args) {
@@ -20,11 +21,25 @@ public class LibraryDriver {
 				"Kodos",
 				"Intergalactic Press",
 				new Date(999999999));
-		System.out.println(book3);
+		// System.out.println(book3);
 		Book book4 = new Book("Stuff and Things",
 				"Mr. Guy",
 				"Book Publishers",
 				new Date(333333333));
-		System.out.println(book4);
+		// System.out.println(book4);
+		LibraryMember member1 = new LibraryMember("Jesse", "Franceschini");
+		LibraryMember member2 = new LibraryMember("Homer", "Simpson");
+		LibraryMember member3 = new LibraryMember("Lisa", "Simpson");
+		LibraryMember member4 = new LibraryMember("Marge", "Simpson");
+		LibraryMember member5 = new LibraryMember("Bart", "Simpson");
+		// System.out.println(member1);
+		member1.checkOutBook(book1);
+		member1.checkOutBook(book2);
+		member2.checkOutBook(book2);
+		member3.checkOutBook(book3);
+		member4.checkOutBook(book4);
+		member5.checkOutBook(book1);
+		// System.out.println(member1);
+		// System.out.println(book1);
 	}
 }
