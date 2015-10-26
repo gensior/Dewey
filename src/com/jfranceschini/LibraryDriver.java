d8eea0cb3fa284a1ba56818a0f8ec0b28979368d
Jesse Franceschini <j.franceschini@gmail.com>
2015-10-25 17:51:10 -0700

    Adds more javadocs and comments.

--- src/com/jfranceschini/LibraryDriver.java
+++ src/com/jfranceschini/LibraryDriver.java
@@ -3,8 +3,23 @@ package com.jfranceschini;
 import com.jfranceschini.library.Book;
 import com.jfranceschini.library.LibraryMember;
 
+/**
+ * Library Driver
+ * 
+ * Interacts with the LibraryMember and Book classes to simulate a library.
+ * 
+ * @author Jesse Franceschini
+ *
+ */
 public class LibraryDriver {
+	/**
+	 * Just the main method to be run in the console
+	 * 
+	 * @param args String array from the command line.
+	 */
 	public static void main(String[] args) {
+		// Instantiate some books
+		// Green Eggs and Ham
 		Book book1 = new Book("Green Eggs and Ham", 
 				"Dr. Seuss", 
 				"Scholastic", 
@@ -12,6 +27,7 @@ public class LibraryDriver {
 				1,
 				10);
 		System.out.println(book1);
+		// Hello World
 		Book book2 = new Book("Hello World",
 				"",
 				"",
@@ -19,28 +35,28 @@ public class LibraryDriver {
 				11,
 				31);
 		System.out.println(book2);
+		// How to Serve Man
 		Book book3 = new Book("How to Serve Man",
 				"Kodos",
 				"Intergalactic Press",
 				1994);
 		System.out.println(book3);
+		// Stuff and Things (tests out no publish date upon instantiation)
 		Book book4 = new Book("Stuff and Things",
 				"Mr. Guy",
 				"Book Publishers");
 		System.out.println(book4);
+		// Instantiate some LibraryMembers
 		LibraryMember member1 = new LibraryMember("Jesse", "Franceschini");
 		LibraryMember member2 = new LibraryMember("Homer", "Simpson");
 		LibraryMember member3 = new LibraryMember("Lisa", "Simpson");
 		LibraryMember member4 = new LibraryMember("Marge", "Simpson");
 		LibraryMember member5 = new LibraryMember("Bart", "Simpson");
-		// System.out.println(member1);
 		member1.checkOutBook(book1);
 		member1.checkOutBook(book2);
 		member2.checkOutBook(book2);
 		member3.checkOutBook(book3);
 		member4.checkOutBook(book4);
 		member5.checkOutBook(book1);
-		// System.out.println(member1);
-		// System.out.println(book1);
 	}
 }
--- src/com/jfranceschini/library/LibraryMember.java
+++ src/com/jfranceschini/library/LibraryMember.java
@@ -1,28 +1,68 @@
 package com.jfranceschini.library;
 
+/**
+ * Library Member
+ * @author Jesse Franceschini
+ *
+ */
 public class LibraryMember {
+	/**
+	 * First name of library member
+	 */
 	private String firstName;
+	/**
+	 * Last name of library member
+	 */
 	private String lastName;
+	/**
+	 * Book that the library member has checked out
+	 */
 	private Book bookCheckedOut;
 	
+	/**
+	 * Constructor for the LibraryMember
+	 * 
+	 * By default, LibraryMembers cannot have a book checked out when they are created.
+	 * 
+	 * @param firstName String of the first name
+	 * @param lastName String of the last name
+	 */
 	public LibraryMember(String firstName, String lastName) {
+		// Sets the first name of the library member
 		this.firstName = firstName;
+		// Sets the last name of the library member
 		this.lastName = lastName;
+		// By default, no book can be checked out on instantiation
 		this.bookCheckedOut = null;
 	}
 	
+	/**
+	 * Checks out a book for the LibraryMember
+	 * 
+	 * Books can only be checked out if they aren't already checked out.
+	 * LibraryMembers can only check out one book at a time.
+	 * 
+	 * @param book the Book object to be checked out
+	 */
 	public void checkOutBook(Book book) {
+		// If the book is not checked out and the LibraryMember has no book checked out
 		if (bookCheckedOut == null && !book.isCheckedOut()) {
+			// Set the book in the LibraryMember
 			setBookCheckedOut(book);
+			// Set the book's checked out status
 			book.setCheckedOut(true);
+			// Register the LibraryMember with the book so it knows who checked it out
 			book.setCheckerOuter(this);
 			System.out.println(firstName + " " + lastName + " checked out \"" + book.getTitle() + "\".");
 		} else {
+			// Otherwise the book can't be checked out
 			System.out.println(firstName + " " + lastName + " failed to check out \"" + book.getTitle() + "\".");
 			if (bookCheckedOut != null) {
+				// Because the LibraryMember already has a book checked out
 				System.out.println(firstName + " " + lastName + " must return \"" + bookCheckedOut.getTitle() + "\" first.");
 			}
 			if (book.isCheckedOut()) {
+				// Or because the book's been checked out by another LibraryMember
 				System.out.println(book.getCheckerOuter().getFirstName() + " " +
 					book.getCheckerOuter().getLastName() + " has already checked out \"" +
 					book.getTitle() + "\".");
@@ -30,30 +70,63 @@ public class LibraryMember {
 		}
 	}
 
+	/**
+	 * Gets the first name of the LibraryMember
+	 * 
+	 * @return a String of the first name
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
+	/**
+	 * Sets the first name of the LibraryMember
+	 * 
+	 * @param firstName a String of the first name
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * Gets the last name of the LibraryMember
+	 * 
+	 * @return a String of the last name
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * Sets the LibraryMember's last name
+	 * 
+	 * @param lastName a String of the last name
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	/**
+	 * Gets the checked out book
+	 * 
+	 * @return a Book that the LibraryMember has checked out
+	 */
 	public Book getBookCheckedOut() {
 		return bookCheckedOut;
 	}
 
+	/**
+	 * Sets the checked out book
+	 * 
+	 * @param bookCheckedOut a Book to be checked out
+	 */
 	public void setBookCheckedOut(Book bookCheckedOut) {
 		this.bookCheckedOut = bookCheckedOut;
 	}
 
+	/**
+	 * Allows for tailored representation to the console.
+	 */
 	@Override
 	public String toString() {
 		String output = "*******LibraryMember*******" +
